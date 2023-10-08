d034dc95582ea4f3e3355a31396da1e4cc63a3e6
조신연 <choshinyoun@naver.com>
2023-10-08 22:43:09 +0900

    component generic content projection

---
+++ BlazorServerTutorial/Data/IPerson.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorServerTutorial.Data;
+
+public interface IPerson
+{
+    string Name { get; set; }
+}
+
+public class Student : IPerson
+{
+    public string Name { get; set; } = "";
+}
+
+public class Teacher : IPerson
+{
+    public string Name { get; set; } = "";
+}
