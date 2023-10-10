7c33077dce05a1364c053c09129fcd444bbb9856
조신연 <choshinyoun@naver.com>
2023-10-10 20:49:31 +0900

    event lambda

---
+++ BlazorServerTutorial/Data/CustomEvents.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerTutorial.Data;
+
+[EventHandler("oncustompaste", typeof(CustomPasteEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+public static class EventHandlers
+{
+}
+
+public class CustomPasteEventArgs : EventArgs
+{
+    //public DateTime EventTimestamp { get; set; }
+    //public string? PastedData { get; set; }
+    //public bool IsMultimedia { get; set; }
+    public bool IsMultimedia { get; set; }
+    public DateTime EventTimestamp { get; set; }
+    public string? PastedData { get; set; }
+}
